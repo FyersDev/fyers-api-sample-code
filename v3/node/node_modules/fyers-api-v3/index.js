8514f5865c89f32258008320e0922162f97c0dad
nihalfyers <nihal.suthar@fyers.in>
2023-08-18 17:54:18 +0530

    node package and venv removed

--- v3/node/node_modules/fyers-api-v3/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var fyersModel =require("./apiService/apiService.js");
-var fyersDataSocket =require("./HSM/datasocket.min.js");
-var fyersOrderSocket =require("./ordersocket/fyersSocket.js");
-
-module.exports.fyersModel = fyersModel;
-module.exports.fyersDataSocket = fyersDataSocket;
-module.exports.fyersOrderSocket = fyersOrderSocket;
